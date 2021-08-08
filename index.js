61105c5c3f56e3e02bc10f26a6bb80c40c356705
N <1356007565@qq.com>
2021-08-08 18:09:11 +0800

    update

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 const scope_flag = 'data-fd-scope';
 
-module.exports = function (originStr){
-  if ( !originStr || !originStr.trim() ) { return ''; }
-  
+module.exports = function (originStr=""){
   originStr = originStr.trim();
+  if ( !originStr ) { return ''; }
+  
   let rgep = /^\s*?\/\*.*?scope\s*?:\s*(.*?);?\s*\*\//s;
   let rst01 = rgep.exec(originStr) || [];
   let scopeName = rst01[1];
-  
   // 不启用命名空间 
   if ( !scopeName ) { return originStr; }
   
-  rgep = /\/\*.*?\*\//gms;
-  originStr = originStr.replace(rgep, '');
   let scope_attr_selector = `[${scope_flag}="${scopeName}"]`;
-  // 所有属性选择器增加命名空间  
-  rgep = /([^:])(\s+?)(\[.+?\])(\s+?)([^:])/gms;
-  originStr = originStr.replace(rgep, `$1$2${scope_attr_selector}$3$4$5`);
-  // 所有标签选择器增加命名空间  
-  rgep = /([^:])(\s+?)([a-zA-Z\-\_]+?)(\s+?)([^:])/gms;
-  originStr = originStr.replace(rgep, `$1$2${scope_attr_selector}$3$4$5`);
-  // 所有class选择器增加命名空间 
-  rgep = /([^:])(\s+?)(\.[a-zA-Z0-9\-\_]+?\s+?)([^:])/gms;
-  originStr = originStr.replace(rgep, `$1$2${scope_attr_selector}$3$4`);
-  // 多次 查漏补缺 待优化 
-  originStr = originStr.replace(rgep, `$1$2${scope_attr_selector}$3$4`); 
+  // 去掉注释
+  rgep = /\/\*.*?\*\//gms;
+  originStr = originStr.replace(rgep, `\n`);
+  // 格式化 
+  rgep = /(\})/gms;
+  originStr = originStr.replace(rgep, `$1\n`);
+  // 属性选择器 增加命名空间  
+  rgep = /(\n\s*?)(\[.+?\])([^\{\}]*?\{.*?\})/gms;
+  originStr = originStr.replace(rgep, `$1${scope_attr_selector}$2$3`);
+  // 标签选择器 增加命名空间  
+  rgep = /(\n\s*?)([a-zA-Z\-\_]+?)([^\{\}]*?\{.*?\})/gms;
+  originStr = originStr.replace(rgep, `$1${scope_attr_selector}$2$3`);
+  // class选择器 增加命名空间 
+  rgep = /(\n\s*?)(\.[a-zA-Z0-9\-\_]+?)([^\{\}]*?\{.*?\})/gms;
+  originStr = originStr.replace(rgep, `$1${scope_attr_selector}$2$3`);
   // * 选择器替换 
   rgep = /\*/gms;
   originStr = originStr.replace(rgep, `${scope_attr_selector}`);
   
-  return originStr;
+  return `/* ${scopeName} scoped */ \n ${originStr}`;
 } 
